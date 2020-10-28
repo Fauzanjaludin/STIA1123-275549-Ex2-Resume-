7e76ef21cea0b8cacc6de8d983fd87c2f5887e7e
Fauzanjaludin <73268282+Fauzanjaludin@users.noreply.github.com>
2020-10-28 15:43:20 +0800

    test2

--- Resume/src/a/main.java
+++ Resume/src/a/main.java
@@ -4,29 +4,7 @@ package a;
 public class main {
 
 	public static void main(String[] args) {
-		/*
-		 * System.out.println("RESUME \n\r");
-       System.out.println("NAME                 : AG MOHD FAUZAN BIN JALUDIN ");
-       System.out.println("GENDER               : MALE");
-       System.out.println("DATE OF BIRTH        : 26 JUNE 1998 ");
-       System.out.println("AGE                  : 22 YEARS OLD");
-       System.out.println("MARITAL STATUS       : SINGLE ");
-       System.out.println("CITIZENSHIP          : MALAYSIA ");
-       System.out.println("HOME ADDRESS         : JLN PADANG DAUN,PAPAR,SABAH ");
-       System.out.println("HANDPHONE NO.        : 01131996497 ");
-       System.out.println("EXAM RESULT(STIA1113): A  ");
-       System.out.println("CGPA                 : 3.81 ");
-       System.out.println("CGPA                 : " + cgpa1);
-       System.out.println("WORK EXPERIENCE      : 1.MARKETING ADMIN ");
-       System.out.println("                       2.CASHIER\n ");
-       System.out.println("LANGUAGE             : 1.MALAY(90%) ");
-       System.out.println("                       2.ENGLISH(80%) \n");
-       System.out.println("PROFESSIONAL SKILLS  : 1.MS PACKAGE(90%) ");
-       System.out.println("                       2.ADOBE PHOTOSHOP(60%)");
-       System.out.println("                       3.VIDEO EDITING(40%)\n");
-       System.out.println("AWARDS & CERTIFICATE : 1.MALAYSIAN UNIVERSITY ENGLISH TEST - BAND 4 ");
-       System.out.println("                       2.POLYTECHNIC DEAN'S AWARD - SEM 2&3");
-		 */
+		
 	   //biodata, work skills, work experiences, talents, programming language results, and CGPA calculation.
 		Biodata b= new Biodata();//includes name,gender,dob,age,marital status,citizenship,home address,handphone no
 		Resultsprog r= new Resultsprog();
